c9bdc9234df5b74edc7ecd3cc842f37af3e7a694
thainasilva1102@gmail.com <thainasilva1102@gmail.com>
2024-09-08 16:24:18 -0300

    m1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// Função para mostrar o alerta
-function showAlert() {
-    document.getElementById('alert').classList.remove('hidden');
-}
-
-// Função para esconder o alerta
-function hideAlert() {
-    document.getElementById('alert').classList.add('hidden');
-}
-
-// Mostrar o alerta automaticamente quando a página é carregada
 window.onload = function() {
-    showAlert();
+    alert('Welcome to my Portfolio!');
 };
